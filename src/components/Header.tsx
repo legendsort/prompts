49c9e1a775ee6b83eb10f1f8ed594b31610c0606
webmiraclepro <webmiraclepro@gmail.com>
2023-03-08 07:54:20 -0500

    fix header content style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,23 +11,25 @@ export default function Header() {
       </div>
 
       <div className="grow flex items-center px-5 border-r-[0.5px] border-[#FFFFFF66]">
-        <div className="grow flex items-center px-4 py-3 mr-9 border-[0.5px] border-[#FFFFFF99] rounded-full">
-          <input
-            className="grow bg-transparent outline-none placeholder:text-white placeholder:text-sm placeholder:leading-4"
-            type="text"
-            placeholder="Search Prompts..."
-          />
-          <Image src="/search.svg" alt="search" width="20" height="20" />
-        </div>
+        <div className="container flex items-center mx-auto">
+          <div className="grow flex items-center px-4 py-3 mr-9 border-[0.5px] border-[#FFFFFF99] rounded-full">
+            <input
+              className="grow bg-transparent outline-none placeholder:text-white placeholder:text-sm placeholder:leading-4"
+              type="text"
+              placeholder="Search Prompts..."
+            />
+            <Image src="/search.svg" alt="search" width="20" height="20" />
+          </div>
 
-        <nav className="flex opacity-70">
-          {ROUTE_MAP.map(({ title, href, icon }) => (
-            <Link key={title} className="flex items-center mr-6 last:mr-0" href={href}>
-              {title}
-              {icon && <Image className="ml-1" src={icon} alt={title} width="8" height="8" />}
-            </Link>
-          ))}
-        </nav>
+          <nav className="flex opacity-70">
+            {ROUTE_MAP.map(({ title, href, icon }) => (
+              <Link key={title} className="flex items-center mr-6 last:mr-0" href={href}>
+                {title}
+                {icon && <Image className="ml-1" src={icon} alt={title} width="8" height="8" />}
+              </Link>
+            ))}
+          </nav>
+        </div>
       </div>
 
       <div className="flex items-center px-9">
