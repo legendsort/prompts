bebbbc325a452c29ff88c8b79338bca8da4ddb61
webmiraclepro <webmiraclepro@gmail.com>
2023-03-08 12:17:15 -0500

    fix body back

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,13 @@ import Footer from '@/components/Footer';
 
 export default function Layout({ children }: { children: JSX.Element }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-
-      <div className="grow">{children}</div>
-
-      <Footer />
+    <div className="flex flex-col min-h-screen relative">
+      <div className="absolute opacity-10 left-0 top-0 w-full h-full bg-[url('/back.jpeg')]" />
+      <div className="relative">
+        <Header />
+        <div className="grow">{children}</div>
+        <Footer />
+      </div>
     </div>
   );
 }
