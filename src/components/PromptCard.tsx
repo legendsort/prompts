9e5c3765e0b8f60bf99de1c4ff8c5e280885b3f6
webmiraclepro <webmiraclepro@gmail.com>
2023-03-08 12:04:24 -0500

    fix prompt card desc style

--- src/components/PromptCard.tsx
+++ src/components/PromptCard.tsx
@@ -7,15 +7,15 @@ export default function PromptCard({ title, price, tag, image, rating }: PromptC
 
   return (
     <div
-      className="flex flex-col w-[270px] h-[240px] p-3.5 bg-no-repeat bg-cover border-2 border-[#FFFFFF4D] rounded-lg"
+      className="flex flex-col w-[270px] h-[240px] bg-no-repeat bg-cover border-2 border-[#FFFFFF4D] rounded-lg"
       style={{ backgroundImage: `url('${image}')` }}
     >
-      <div className="flex justify-between" style={{ flexDirection: getDirection(rating) }}>
+      <div className="flex justify-between p-3.5" style={{ flexDirection: getDirection(rating) }}>
         <TagCard type={tag} />
         {rating && <TagCard type="rating" value={rating.toString()} />}
       </div>
 
-      <div className="flex w-full h-1/2 mt-auto items-end">
+      <div className="flex items-end w-full h-1/2 p-3.5 mt-auto bg-gradient-to-b from-[#00000000] to-black">
         <span className="grow mr-5">{title}</span>
         <span className="font-medium text-xl leading-6">${price}</span>
       </div>
