ebe6131e453cbd193e909bc5f0f7f78365d90eaa
webmiraclepro <webmiraclepro@gmail.com>
2023-03-08 08:13:56 -0500

    complete home featured in

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -54,3 +54,48 @@ export const ROUTE_MAP = [
     icon: '/right-arrow.svg',
   },
 ];
+
+export const FEATURED_MAP = [
+  {
+    title: 'wsj',
+    image: '/wsj.svg',
+    width: 85,
+    height: 40,
+  },
+  {
+    title: 'yahoo',
+    image: '/yahoo.svg',
+    width: 290,
+    height: 40,
+  },
+  {
+    title: 'ft',
+    image: '/ft.svg',
+    width: 390,
+    height: 30,
+  },
+  {
+    title: 'atlantic',
+    image: '/atlantic.svg',
+    width: 85,
+    height: 30,
+  },
+  {
+    title: 'fastcompany',
+    image: '/fastcompany.svg',
+    width: 200,
+    height: 30,
+  },
+  {
+    title: 'wired',
+    image: '/wired.svg',
+    width: 150,
+    height: 30,
+  },
+  {
+    title: 'verge',
+    image: '/verge.svg',
+    width: 175,
+    height: 30,
+  },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import { FEATURED_MAP } from '@/helpers/constants';
 
 export default function Home() {
   return (
@@ -10,7 +12,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <div className="flex flex-col items-center mb-36">
+          <h2 className="mb-9">Featured In</h2>
+
+          <div className="flex flex-wrap max-w-[1005px] gap-y-8 -mr-20">
+            {FEATURED_MAP.map(({ title, image, width, height }) => (
+              <Image key={title} className="mr-20 last:mr-0" src={image} alt={title} width={width} height={height} />
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
