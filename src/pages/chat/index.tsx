7fd957e84def4689c5530f05dcd33dc4ee02a8a5
webmiraclepro <webmiraclepro@gmail.com>
2023-03-09 14:57:19 -0500

    fix: chat ui

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import Icon from '@/components/Icon';
 import User from '@/components/User';
 import { users, messages } from '@/helpers/mock';
@@ -7,11 +8,11 @@ const Chat: NextPage = () => {
   return (
     <>
       <div className="bg-[#222236] w-[1140px] h-[776px] rounded-lg mx-auto my-12 overflow-auto">
-        <div className="grid grid-cols-10 gap-2 h-full">
+        <div className="grid grid-cols-10 h-full">
           <div className="col-span-3 border-r-2 border-[#FFFFFF] border-opacity-10 h-full scroll-auto">
             <div className="flex flex-col px-4 py-4 space-y-2">
-              <p>InMail</p>
-              <div className="grow flex bg-[#515151] items-center px-4 py-3 mr-9 border-[0.5px] border-[#FFFFFF99] rounded-full">
+              <p className="text-gray-400">InMail</p>
+              <div className="grow flex bg-[#515151] items-center px-4 py-2 mr-9 border-[0.5px] border-[#FFFFFF99] rounded-full">
                 <Icon>search</Icon>
                 <input
                   className="grow bg-[#515151] outline-none placeholder:text-white placeholder:text-sm placeholder:leading-4"
@@ -27,8 +28,17 @@ const Chat: NextPage = () => {
             </div>
           </div>
 
-          <div className="col-span-7 px-4">
-            <div className="flex flex-col gap-8">
+          <div className="col-span-7">
+            <div className="flex items-center bg-[#4CDE55] h-[60px] w-full px-4 mb-6">
+              <div className="flex flex-row gap-4 items-center">
+                <Image src="/avatars/avatar1.png" alt="avatar" width="41" height="41" className="rounded-full" />
+                <div className="flex flex-col text-sm text-black">
+                  <p className="font-bold">John Dae</p>
+                  <p className="text-xs">Typing...</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-6 px-4">
               {messages.map(({ avatar, message, timeStamp, type }, index) =>
                 type === 'other' ? (
                   <div key={index} className="mr-auto">
@@ -41,7 +51,7 @@ const Chat: NextPage = () => {
                 ),
               )}
             </div>
-            <div className="grow flex bg-[#515151] items-center px-4 py-1 items-center border-[0.5px] border-[#FFFFFF99] rounded-full">
+            <div className="grow flex bg-[#515151] items-center px-4 py-1 items-center border-[0.5px] border-[#FFFFFF99] rounded-full mx-4">
               <input
                 className="grow bg-[#515151] outline-none placeholder:text-gray-300 placeholder:text-sm placeholder:leading-4"
                 type="text"
